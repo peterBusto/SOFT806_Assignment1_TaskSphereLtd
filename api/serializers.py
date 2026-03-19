cd8ff408bddfda9030fc25af5c1d6d64f2893fd3
Peter <pebu001@ess.ais.ac.nz>
2026-03-19 23:12:13 +1300

    Database setup completed.

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,51 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Project, Task
+from .models import Project, Task, UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['bio', 'phone_number', 'timezone', 'company', 'position', 'is_verified']
+        read_only_fields = ['is_verified']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+    profile = UserProfileSerializer(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password_confirm', 'profile']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError("Passwords don't match.")
+        return attrs
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile', {})
+        validated_data.pop('password_confirm')
+        
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
+        )
+        
+        UserProfile.objects.create(user=user, **profile_data)
+        return user
 
 
 class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'profile']
 
 
 class ProjectSerializer(serializers.ModelSerializer):
